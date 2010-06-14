a875d28102e2bc9ef7704710cae5b86f4c319565
Chris Mulligan <chris.mulligan@gmail.com>
2010-06-13 22:58:31 -0400

    0.0.2 - better setup.py

--- lpaste/setup.py
+++ lpaste/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="lpaste",
-	version="0.0.1",
+	version="0.0.2",
 	packages=['lpaste'],
 	entry_points = {
 		'console_scripts': [
@@ -11,5 +11,20 @@ setup(name="lpaste",
 	install_requires = [
 		'poster', 
 	],
+    description="IRC bot - full featured, yet extensible and customizable",
+    license = 'MIT',
+    author="Chris Mulligan",
+    author_email="chmullig@gmail.com",
+    maintainer = 'Chris Mulligan',
+    maintainer_email = 'chmullig@gmail.com',
+    url = 'http://bitbucket.org/chmullig/librarypaste-tools/lpaste',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
 
